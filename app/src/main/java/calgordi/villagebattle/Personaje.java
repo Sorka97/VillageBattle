97c742fc1add7c3a89d9e061fa65e01e5baed4ff
Sergio Romero López <sorka1997@gmail.com>
2017-04-03 12:02:09 +0200

    Cambio2

---
+++ app/src/main/java/calgordi/villagebattle/Personaje.java
@@ -0,0 +1,100 @@
+package calgordi.villagebattle;
+
+import android.media.Image;
+
+public class Personaje {
+    // Propiedades
+    private String nombre;
+    private int vida;
+    private float velocidad;
+    private float campoVision;
+    private String habilidadEspecial;
+
+    // Sprites
+    private Image icono;
+    // Mirando arriba
+    private Image arribaQuieto;
+    private Image arribaPasoIzquierdo;
+    private Image arribaPasoDerecho;
+    private Image arribaAtacando1;
+    private Image arribaAtacando2;
+    // Mirando izquierda
+    private Image izquierdaQuieto;
+    private Image izquierdaPasoIzquierdo;
+    private Image izquierdaPasoDerecho;
+    private Image izquierdaAtacando1;
+    private Image izquierdaAtacando2;
+    // Mirando derecha
+    private Image derechaQuieto;
+    private Image derechaPasoIzquierdo;
+    private Image derechaPasoDerecho;
+    private Image derechaAtacando1;
+    private Image derechaAtacando2;
+    // Mirando abajo
+    private Image abajoQuieto;
+    private Image abajoPasoIzquierdo;
+    private Image abajoPasoDerecho;
+    private Image abajoAtacando1;
+    private Image abajoAtacando2;
+
+    //Constructor
+    public Personaje (String name, int life, float velocity, float fieldOfView, String specialAbility, Image icon ,Image upQuieto, Image rightQuieto, Image leftQuieto, Image downQuieto, Image upPasoIzquierdo, Image rightPasoIzquierdo, Image leftPasoIzquierdo, Image downPasoIzquierdo, Image upPasoDerecho , Image rightPasoDerecho, Image leftPasoDerecho, Image downPasoDerecho, Image upAtacando1 , Image rightAtacando1 , Image leftAtacando1, Image downAtacando1, Image upAtacando2, Image rightAtacando2, Image leftAtacando2, Image downAtacando2){
+
+        nombre = name;
+        vida = life;
+        velocidad = velocity;
+        campoVision = fieldOfView;
+        habilidadEspecial = specialAbility;
+
+        icono =  icon;
+        arribaQuieto = upQuieto;
+        abajoQuieto = downQuieto;
+        derechaQuieto = rightQuieto;
+        izquierdaQuieto = leftQuieto;
+
+        arribaPasoIzquierdo = upPasoIzquierdo;
+        abajoPasoIzquierdo = downPasoIzquierdo;
+        derechaPasoIzquierdo = rightPasoIzquierdo;
+        izquierdaPasoIzquierdo = leftPasoIzquierdo;
+
+        arribaPasoDerecho = upPasoDerecho;
+        abajoPasoDerecho = downPasoDerecho;
+        derechaPasoDerecho = rightPasoDerecho;
+        izquierdaPasoDerecho = leftPasoDerecho;
+
+
+        arribaAtacando1 = upAtacando1;
+        abajoAtacando1 = downAtacando1;
+        derechaAtacando1 = rightAtacando1;
+        izquierdaAtacando1 = leftAtacando1;
+
+        arribaAtacando2 = upAtacando2;
+        abajoAtacando2 = downAtacando2;
+        derechaAtacando2 = rightAtacando2;
+        izquierdaAtacando2 = leftAtacando2;
+
+    }
+//Metodos set - get
+
+    public String getNombre(){
+        return nombre;
+    }
+    public void setVida(int life){
+        vida = life;
+    }
+    public int getVida() {
+        return vida;
+    }
+    public void setVelocidad (float velocity){
+        velocidad = velocity;
+    }
+    public float getVelocidad () {
+        return velocidad;
+    }
+    public void setCampoVision(float fieldOfView) {
+        campoVision = fieldOfView;
+    }
+    public float getCampoVision(){
+        return campoVision;
+    }
+}
