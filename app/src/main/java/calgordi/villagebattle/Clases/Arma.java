7213e9481fd996438bd5c2242719d6f9c8a74808
Sergio Romero López <sorka1997@gmail.com>
2017-04-03 15:07:53 +0200

    Cambio2

--- app/src/main/java/calgordi/villagebattle/Clases/Arma.java
+++ app/src/main/java/calgordi/villagebattle/Clases/Arma.java
@@ -1,10 +1,46 @@
 package calgordi.villagebattle.Clases;
 
+import android.media.Image;
+
 public class Arma {
     private String nombre;
     private String tipo; // Si es a "melee" o a "distance"
     private int nivel; // Para subir de nivel hay que gastar oro y experiencia
     private int experiencia;
-    private int dano;
+    private float dano;
+    private float velocidadAtaque;
+    private int municionCargador; // Si es de type melee este dato no importara
+    private float tiempoRecarga;
     private String habilidadEspecial;
+
+    private Image bala;
+
+    public Arma (String name, String type, int level, int experience, float damage, float attackSpeed, int ammo, float reload, String specialAbility, Image bullet){
+        nombre = name;
+        tipo = type;
+        nivel = level;
+        experiencia = experience;
+        dano = damage;
+        velocidadAtaque = attackSpeed;
+        municionCargador = ammo;
+        tiempoRecarga = reload;
+        habilidadEspecial = specialAbility;
+        bala = bullet;
+    }
+
+    public String getNombre (){return nombre;}
+    public String getTipo () {return tipo;}
+    public void setNivel (int level){ nivel = level;}
+    public int getNivel (){ return nivel;}
+    public void setExperiencia (int experience) {experiencia = experience;}
+    public int getExperiencia(){return experiencia;}
+    public void setDano(float damage){ dano = damage;}
+    public float getDano (){return dano;}
+    public void setVelocidadAtaque (float attackSpeed) {velocidadAtaque = attackSpeed;}
+    public float getVelocidadAtaque (){return velocidadAtaque;}
+    public void setMunicionCargador (int ammo) {municionCargador = ammo;}
+    public int getMunicionCargador (){return municionCargador;}
+    public void setTiempoRecarga (float reload){tiempoRecarga = reload;}
+    public float getTiempoRecarga (){return tiempoRecarga;}
+    public String getHabilidadEspecial () {return habilidadEspecial;}
 }
--- app/src/main/java/calgordi/villagebattle/Clases/Personaje.java
+++ app/src/main/java/calgordi/villagebattle/Clases/Personaje.java
@@ -13,37 +13,19 @@ public class Personaje {
 
     // Sprites
     private Image icono;
-    // Mirando arriba
-    private Image arribaQuieto;
-    private Image arribaPasoIzquierdo;
-    private Image arribaPasoDerecho;
-    private Image arribaAtacando1;
-    private Image arribaAtacando2;
-    // Mirando izquierda
-    private Image izquierdaQuieto;
-    private Image izquierdaPasoIzquierdo;
-    private Image izquierdaPasoDerecho;
-    private Image izquierdaAtacando1;
-    private Image izquierdaAtacando2;
-    // Mirando derecha
-    private Image derechaQuieto;
-    private Image derechaPasoIzquierdo;
-    private Image derechaPasoDerecho;
-    private Image derechaAtacando1;
-    private Image derechaAtacando2;
-    // Mirando abajo
-    private Image abajoQuieto;
-    private Image abajoPasoIzquierdo;
-    private Image abajoPasoDerecho;
-    private Image abajoAtacando1;
-    private Image abajoAtacando2;
+
+    private Image quieto;
+    private Image pasoIzquierdo;
+    private Image pasoDerecho;
+    private Image atacando;
+    private Image atacando2;
 
     //Sonido
     private MediaPlayer sonidoPersonaje;
     private MediaPlayer sonidoPasos;
 
     //Constructor
-    public Personaje (String name, int life, float velocity, float fieldOfView, String specialAbility, Image icon ,Image upQuieto, Image rightQuieto, Image leftQuieto, Image downQuieto, Image upPasoIzquierdo, Image rightPasoIzquierdo, Image leftPasoIzquierdo, Image downPasoIzquierdo, Image upPasoDerecho , Image rightPasoDerecho, Image leftPasoDerecho, Image downPasoDerecho, Image upAtacando1 , Image rightAtacando1 , Image leftAtacando1, Image downAtacando1, Image upAtacando2, Image rightAtacando2, Image leftAtacando2, Image downAtacando2, MediaPlayer soundPersonaje, MediaPlayer soundPasos){
+    public Personaje (String name, int life, float velocity, float fieldOfView, String specialAbility, Image icon ,Image qieto, Image pasoIzqierdo, Image pasoDrecho ,Image atcando,Image atcando2, MediaPlayer soundPersonaje, MediaPlayer soundPasos){
 
         nombre = name;
         vida = life;
@@ -52,31 +34,11 @@ public class Personaje {
         habilidadEspecial = specialAbility;
 
         icono =  icon;
-        arribaQuieto = upQuieto;
-        abajoQuieto = downQuieto;
-        derechaQuieto = rightQuieto;
-        izquierdaQuieto = leftQuieto;
-
-        arribaPasoIzquierdo = upPasoIzquierdo;
-        abajoPasoIzquierdo = downPasoIzquierdo;
-        derechaPasoIzquierdo = rightPasoIzquierdo;
-        izquierdaPasoIzquierdo = leftPasoIzquierdo;
-
-        arribaPasoDerecho = upPasoDerecho;
-        abajoPasoDerecho = downPasoDerecho;
-        derechaPasoDerecho = rightPasoDerecho;
-        izquierdaPasoDerecho = leftPasoDerecho;
-
-
-        arribaAtacando1 = upAtacando1;
-        abajoAtacando1 = downAtacando1;
-        derechaAtacando1 = rightAtacando1;
-        izquierdaAtacando1 = leftAtacando1;
-
-        arribaAtacando2 = upAtacando2;
-        abajoAtacando2 = downAtacando2;
-        derechaAtacando2 = rightAtacando2;
-        izquierdaAtacando2 = leftAtacando2;
+        quieto = qieto;
+        pasoIzquierdo = pasoIzqierdo;
+        pasoDerecho = pasoDrecho;
+        atacando = atcando;
+        atacando2 = atcando2;
 
         sonidoPersonaje = soundPersonaje;
         sonidoPasos = soundPasos;
